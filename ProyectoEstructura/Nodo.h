dba727169343cd3ccadb68b0d76a177e0eb0dd69
Zadropol <aaadrianrodriguezmontecinos@gmail.com>
2025-11-25 15:48:10 -0400

    yo menos

--- ProyectoEstructura/Nodo.h
+++ ProyectoEstructura/Nodo.h
@@ -3,7 +3,6 @@
 #include <vector>	
 #include <string>
 
-#define INF 1e9
 using namespace std;
 
 template <class T>
@@ -13,11 +12,9 @@ class Nodo
 public:
 	bool marca;
 	string padre;
-	int dist;
-	vector<pair<int, T>> lista;
 	Nodo();
 	Nodo(T padre);
-	void eliminarAdyacentes(T e);
+
 };
 
 template<class T>
@@ -30,20 +27,7 @@ template<class T>
 inline Nodo<T>::Nodo(T padre)
 {
 	this->marca = false;
-	this->dist = INF;
 	this->padre = "";
 }
 
-template<class T>
-inline void Nodo<T>::eliminarAdyacentes(T e)
-{
-	int last = lista.size() - 1;
 
-	for (int i = 0; i <= lista.size(); i++) {
-		if (lista[i].second == e) {
-			swap(lista[i], lista[last]);
-			lista.pop_back();
-			break;
-		}
-	}
-}
